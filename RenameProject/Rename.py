f56885421b9e77bfe206917b601ab3c54718d53b
Minephliss <endermanemail@gmail.com>
2020-10-24 13:52:10 +0800

    upgrade

--- RenameProject/Rename.py
+++ RenameProject/Rename.py
@@ -1,5 +1,6 @@
 import os
 import random
+import shutil
 
 def change(value):
     if value > 9:
@@ -7,13 +8,19 @@ def change(value):
     return str(value)
 
 dirname = './' + input('Input the directory\'s name that you are about to operate:') + '/'
+basename = input('The target name is:')
 
 filelist = os.listdir(dirname)
+
+print(filelist)
+
 for file in filelist:
     tarname = dirname
     for i in range(0, 32):
         tarname += change(int(random.random() * 100) % 16)
     tarname += '.jpg'
     os.rename(dirname + file, tarname)
+
+shutil.make_archive('./' + basename,'zip', './', dirname)
 
 print ('Done!')
