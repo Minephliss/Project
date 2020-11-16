b3870a778fad4c447eb452b8c254218b273bf6fe
Minephliss <endermanemail@gmail.com>
2020-11-16 22:37:53 +0800

    New project

---
+++ 合金计算器/a_calculater.cpp
@@ -0,0 +1,24 @@
+#include "c_calculater.h"
+#include <vector>
+/*Metal::Metal(istream& is){
+    cout << "Input the metal\'s information please.\neg. MetalName MetalContent MetalQuantity\n";
+    is >> name >> content >> quantity;
+}
+
+Metal::Metal(const string &n, const int &c, const int &q){
+    name = n;
+    content = c;
+    quantity = q;
+}
+
+struct a_calculater
+{
+    
+};
+*/
+vector<Metal> metalList;
+
+sortOut();
+
+
+printAnswer();
