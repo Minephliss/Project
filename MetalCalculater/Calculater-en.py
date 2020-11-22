4fbf1c2e49a03af3132158f4779f586c8464ecba
Minephliss <endermanemail@gmail.com>
2020-11-22 22:29:50 +0800

    Update version 1.0.2

--- MetalCalculater/Calculater-en.py
+++ MetalCalculater/Calculater-en.py
@@ -1,3 +1,4 @@
+# ===============================================================================================
 class Metal:
     def __init__(self, lst):
         self.name = str(lst[0])
@@ -7,6 +8,8 @@ class Metal:
         self.upperLimit = float(lst[4])
         self.used = int(lst[2])
 
+
+# ===============================================================================================
 metalSerialNumber = []
 kindOfMetal = 0
 metalList = []
@@ -14,8 +17,31 @@ targetQuantity = 0.0
 total = 0.0
 globalJudge = False
 targetName = ""
-version = '1.0.1'
-date = '11/20/2020'
+version = '1.0.2'
+date = '11/22/2020'
+inventory = 0.0
+maxQuantity = 0.0
+# ===============================================================================================
+
+
+def ran(percent, lowerLimit, upperLimit):
+    return percent <= upperLimit and percent >= lowerLimit
+
+
+def main():
+    welcome()
+    inputInformation()
+    calculate()
+
+
+def init():
+    global metalSerialNumber, globalJudge, total, metalList
+    metalSerialNumber = []
+    globalJudge = False
+    total = 0.0
+    for i in metalList:
+        i.used = int(i.quantity)
+
 
 def welcome():
     print("===============================================")
@@ -25,9 +51,34 @@ def welcome():
     print("           Last update: {}".format(date))
     print("===============================================")
 
+
+def printAnswer():
+    if globalJudge:
+        print("The best idea is:\n")
+        for i in metalList:
+            print("{}: {} Percent:{:.2f}%".format(
+                i.name, i.used, i.used * i.content / total * 100))
+        print("\nYou can get {} {} in total.".format(int(total), targetName))
+    else:
+        print("That is impossible!")
+
+
+def calculate():
+    global targetQuantity, inventory, maxQuantity
+    contu = True
+    while(contu):
+        enumerate(0)
+        printAnswer()
+        if input('Other quantity?(Y/N)') == 'N':
+            contu = False
+        else:
+            targetQuantity = float(input("Now you have {:.2f} in total and maybe can produce {:.2f} at most, how much do you want to produce:".format(inventory, maxQuantity)))
+            init()
+    print("Press Enter to exit...")
+
+
 def inputInformation():
-    global kindOfMetal, targetQuantity, targetName
-    inventory = 0.0
+    global kindOfMetal, targetQuantity, targetName, inventory, maxQuantity
     targetName = input("What's the alloy:")
     kindOfMetal = int(input("How many kinds of submetal:"))
     print("Now input the submetals and please input the most important metal at first(eg. MetalName MetalContent MetalQuantity MetalLowerLimit MetalUpperLimit)")
@@ -37,11 +88,10 @@ def inputInformation():
         metal = Metal(lst)
         inventory += (metal.content * metal.quantity)
         metalList.append(metal)
-    maxQuantity = min(inventory, metalList[0].content * metalList[0].quantity / metalList[0].lowerLimit)
+    maxQuantity = min(
+        inventory, metalList[0].content * metalList[0].quantity / metalList[0].lowerLimit)
     targetQuantity = float(input("Now you have {:.2f} in total and maybe can produce {:.2f} at most, how much do you want to produce:".format(inventory, maxQuantity)))
 
-def ran(percent, lowerLimit, upperLimit):
-    return percent <= upperLimit and percent >= lowerLimit
 
 def enumerate(key):
     global metalSerialNumber, globalJudge, total
@@ -59,7 +109,7 @@ def enumerate(key):
                 part.append(j * metalList[k].content)
                 tot += j * metalList[k].content
                 k += 1
-            if tot < targetQuantity:    
+            if tot < targetQuantity:
                 metalSerialNumber.pop()
                 continue
             k = 0
@@ -69,7 +119,7 @@ def enumerate(key):
                 if ran(per, metalList[k].lowerLimit, metalList[k].upperLimit):
                     percent.append(per)
                     k += 1
-                else:   
+                else:
                     judge = True
                     break
             if judge:
@@ -84,22 +134,7 @@ def enumerate(key):
                 total = tot
             metalSerialNumber.pop()
 
-def printAnswer():
-    if globalJudge:
-        print("The best idea is:\n")
-        for i in metalList:
-            print("{}: {} Percent:{:.2f}%".format(i.name, i.used, i.used * i.content / total * 100))
-        print("\nYou can get {} {} in total.".format(int(total), targetName))
-    else:
-        print("That is impossible!")
-    input("Press Enter to exit")
-
-
-def main():
-    welcome()
-    inputInformation()
-    enumerate(0)
-    printAnswer()
+# ===============================================================================================
 
 
 if __name__ == "__main__":
